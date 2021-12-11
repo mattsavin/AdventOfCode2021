edf426c1e50ec2038a3f65b0daafe953fa5bc9ff
Matthew Savin <matt_savin@outlook.com>
2021-12-11 00:09:23 +0000

    initial commit

---
+++ Day1/main.py
@@ -0,0 +1,13 @@
+def main():
+    with open("resources/input.txt", "r") as file:
+        depths = file.read()
+        depths = depths.split("\n")
+
+        count = 0
+
+        for i, depth in enumerate(depths):
+            pass
+
+
+if __name__ == "__main__":
+    main()
