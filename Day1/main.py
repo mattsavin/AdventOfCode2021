c32b78c42ba446d4304856694785691ae59bfbdb
Matthew Savin <matt_savin@outlook.com>
2021-12-11 00:41:52 +0000

    Day 1: improve formatting, convert list[str] to list[int]

--- Day1/main.py
+++ Day1/main.py
@@ -1,21 +1,26 @@
 def get_depths():
     with open("resources/input.txt", "r") as file:
-        return file.read().split("\n")
+        return list(map(int, file.read().split("\n")))
 
 
 def part_one():
     depths = get_depths()
-    count = 1
+    count = 0
 
     for i, depth in enumerate(depths):
         if depth > depths[i-1]:
             count += 1
 
-    print(count)
+    print("Part 1:", count)
 
 
 def part_two():
     depths = get_depths()
+
+    for i, depth in enumerate(depths):
+        if 1 < i < 10:
+            sum_depths = depth + depths[i-1] + depths[i-2]
+            print(sum_depths)
 
 
 if __name__ == "__main__":
